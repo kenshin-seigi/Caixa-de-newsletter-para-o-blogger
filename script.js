cbbebb3f51b86611625bf02fbc49b75a68b0ceeb
Kenshin Seigi 😋 <kenshin.seigi@gmail.com>
2025-01-10 00:25:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
+{
+
+if (localStorage.getItem('newsletterEnviado') === 'true') {
+  document.getElementById('newsletter-container').style.display = 'none';
+  document.getElementById('mensagem-obrigado').style.display = 'block';
+} else {
+  document.getElementById('newsletter-container').style.display = 'block';
+}
+
 setTimeout(() => {
-    document.getElementById('newsletter-container').style.display = 'block';
-  }, 2000);
+  document.getElementById('newsletter-container').style.display = 'block';
+}, 2000);
 
-  document.getElementById('fechar').addEventListener('click', () => {
-    document.getElementById('newsletter-container').style.display = 'none';
-  });
+document.getElementById('fechar').addEventListener('click', () => {
+  document.getElementById('newsletter-container').style.display = 'none';
+});
 
-  document.getElementById('form-newsletter').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email-input').value;
-    fetch('https://formspree.io/f/formspree-id', {
-      method: 'POST',
-      body: JSON.stringify({ email }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then((res) => res.json())
-    .then((data) => console.log('E-mail enviado:', data))
-    .catch((err) => console.error('Erro:', err));
+document.getElementById('form-newsletter').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email-input').value;
+  fetch('https://formspree.io/f/xkggpegz', {
+    method: 'POST',
+    body: JSON.stringify({ email }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log('E-mail enviado:', data);
     document.getElementById('newsletter-container').style.display = 'none';
+    document.getElementById('mensagem-obrigado').style.display = 'block';
+    localStorage.setItem('newsletterEnviado', 'true');
+  })
+  .catch((err) => {
+    console.error('Erro:', err);
   });
+});
+
+}
